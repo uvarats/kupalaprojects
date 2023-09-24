2a93ab21f3e3f9ebeface79d6763e247216c5db1
uvarats <uladzislauvarats@gmail.com>
2023-09-24 16:24:06 +0300

    Update symfony to 6.3 Install psalm and php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PER' => true,
+    ])
+    ->setFinder($finder)
+;
